99d2d34734ee7edd5ab9d661c2e7a34350f3a7a5
MichaelSeeburger <39369290+MichaelSeeburger@users.noreply.github.com>
2022-03-25 11:42:00 +0100

    Quick Layout change
    I'm sorry for this sloppy code / no docu

--- randomizer.js
+++ randomizer.js
@@ -24,7 +24,32 @@ function randomizeTerm() {
   return terms[Math.floor(Math.random() * terms.length)];
 }
 
+
+
 function fillCards(superlative, term) {
+  // Get random colour
+  var colors = ['#DFFF00', '#FFBF00', '#FF7F50', '#DE3163', '#9FE2BF', '#FF00FF', '#800080'  ];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  document.getElementById('superlative').style.color = random_color;
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  document.getElementById('term').style.color = random_color; 
+
+
+  // Get random font
+  var fonts = ['Times New Roman', 'Georgia', 'Garamond', 'Arial', 'Verdana', 'Helvetica', 'Courier New', 'Lucida Console', 'Monaco', 'Brush Script MT', 'Lucida Handwriting', 'Copperplate', 'Papyrus' ];
+  var random_font = fonts[Math.floor(Math.random() * fonts.length)];
+  document.getElementById('superlative').style.fontFamily = random_font;
+  var random_font = fonts[Math.floor(Math.random() * fonts.length)];
+  document.getElementById('term').style.fontFamily = random_font;
+
+
+
+
+
   document.getElementById("superlative").innerHTML = superlative;
   document.getElementById("term").innerHTML = term;
+
+  document.getElementById("wrapper").style.height = '60%';
+
+
 }
